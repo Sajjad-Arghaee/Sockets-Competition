f6ea13922ca28105dcbbeec04195be37ac396bb3
sajjad arghaee <arghaei1380@yahoo.com>
2023-01-16 02:14:23 +0330

    report chat history to host

--- core/server.py
+++ core/server.py
@@ -1,8 +1,10 @@
 import socket
 import time
+import threading
 
 HOST = '127.0.0.1'
 PORT = 54122
+CHAT_STATE = [0, 0, 0]
 questions = [
     """1. what's the color of sun?
         a) red
@@ -20,6 +22,18 @@ questions = [
 answers = ['d', 'a']
 scores = [0, 0, 0]
 chat_keys = ['i', 'o', 'p']
+
+
+def chat_listener(s1: socket):
+    global CHAT_STATE
+    msg = s1.recv(1024).decode()
+    print(msg)
+    CHAT_STATE[0] = 1
+    while msg != 'end_chat':
+        msg = s1.recv(1024).decode()
+        print(msg)
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -37,18 +51,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             conn1.sendall(questions[index].encode())
             # conn2.sendall(questions[index].encode())
             # conn3.sendall(questions[index].encode())
+            thread = threading.Thread(target=chat_listener, args=(conn1,))
+            thread.start()
             while t > 0:
                 minutes, secs = divmod(t, 60)
                 timer = '{:02d}:{:02d}'.format(minutes, secs)
                 print(timer, end='\r')
                 time.sleep(1)
                 t -= 1
+
             conn1.sendall('send'.encode())
+            if not CHAT_STATE[0]:
+                answer1 = conn1.recv(1024)
+                if answer1.decode() == answers[index]:
+                    scores[0] += 1
             # conn2.sendall('send'.encode())
             # conn3.sendall('send'.encode())
-            answer1 = conn1.recv(1024)
-            if answer1.decode() == answers[index]:
-                scores[0] += 1
             # conn2.sendall("send".encode())
             # answer2 = conn2.recv(1024)
             # if answer2.decode() == answers[index]:
@@ -61,7 +79,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
             scoreboard = f'player1 : {scores[0]}, player2 : {scores[1]}, player3 : {scores[2]}'
             print(scoreboard)
-            conn1.sendall(scoreboard.encode())
+            if not CHAT_STATE[0]:
+                conn1.sendall(scoreboard.encode())
             # conn2.sendall(scoreboard.encode())
             # conn3.sendall(scoreboard.encode())
             time.sleep(5)
